aac30d929b651c58aeca49136463319fe1a17d90
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-04-19 15:06:47 +0900

    test: fix a typo

--- tests/test_version.rb
+++ tests/test_version.rb
@@ -3,6 +3,6 @@ require '../lib/groonga-benchmark'
 
 class TestVersion < Test::Unit::TestCase
   def test_version
-    assert_equal("0.0.1", GroongaBenchmark::Version)
+    assert_equal("0.0.1", GroongaBenchmark::VERSION)
   end
 end
