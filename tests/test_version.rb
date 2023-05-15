b06d6ef648c39aa1794a4d6e1188f9843d704966
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-05-15 18:15:12 +0900

    Add a test for showing Groonga's version

--- tests/test_version.rb
+++ tests/test_version.rb
@@ -2,7 +2,17 @@ require 'test/unit'
 require '../lib/groonga-benchmark'
 
 class TestVersion < Test::Unit::TestCase
-  def test_version
+  def test_tool_version
     assert_equal("0.0.1", GroongaBenchmark::VERSION)
+  end
+
+  def test_groonga_version
+    groonga = GroongaBenchmark::Server.new()
+    groonga.run()
+    groonga_latest_vserion = xxxx
+
+    assert_equal(groonga_latest_version, groonga.version())
+
+    groonga.shutdown()
   end
 end
